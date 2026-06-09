da6fb2768c52b88bcbdb1b3d1522fb22e5602bbd
Divyanshu Gupta <fireflyindiainfo@gmail.com>
2026-06-09 13:33:31 +0530

    leetcode 9 Palindrome Number

---
+++ leet-code-codebase/PalindromeNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        long reversed = 0;
+        long temp = x;
+
+        while (temp != 0) {
+            int digit = (int) (temp % 10);
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+
+        return (reversed == x);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Solution solution = new Solution();
+        int number = sc.nextInt();
+        boolean result = solution.isPalindrome(number);
+        System.out.println(number + " is a palindrome: " + result);
+    }
+}
