6fd608d46b7ed67f828acd5f031767771cf36775
Divyanshu Gupta <150168350+divyanshugupta0@users.noreply.github.com>
2026-06-16 23:15:17 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Welcome to CodeSynq!
+// JavaScript - Dynamic Programming Language
+// Press Ctrl+Space for IntelliSense suggestions
+
+console.log("Hello from CodeSynq!");
+
+// Example: Variables and Functions
+const greeting = "Welcome to CodeSynq-JavaScript!";
+function showMessage(msg) {
+    console.log(msg);
+}
+
+showMessage(greeting);
